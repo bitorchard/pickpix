4a25d40e73d75827f0af796f2066d1bb58e698ff
ssledzie <ssledzie@gmail.com>
2022-04-15 01:13:06 -0700

    Add nullable on Field

--- pickpix-app/website/pickpix/models.py
+++ pickpix-app/website/pickpix/models.py
@@ -4,6 +4,6 @@ class PickToken(models.Model):
     token_id = models.CharField(max_length=200)
 
 class Pixel(models.Model):
-    token = models.ForeignKey(PickToken, on_delete=models.SET_NULL)
+    token = models.ForeignKey(PickToken, null=True, on_delete=models.SET_NULL)
     pixel_index = models.IntegerField(default=0, unique=True)
     color = models.CharField(max_length=7, default="#FFFFFF")
