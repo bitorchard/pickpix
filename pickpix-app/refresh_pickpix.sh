825a94323b77c59c8755082c68c5ceb529ee7933
ssledzie <ssledzie@gmail.com>
2024-03-11 21:44:50 -0700

    Separate service def, utility scripts, and minor fixes

--- pickpix-app/refresh_pickpix.sh
+++ pickpix-app/refresh_pickpix.sh
@@ -1,4 +1,6 @@
-docker build --tag pickpix-app:123 . --no-cache
-docker save pickpix-app > pickpix-app_image.tar
-microk8s ctr image import pickpix-app_image.tar
-microk8s.kubectl replace -f pickpix-app-deployment.yaml --force
+kubectl delete deployment pickpix-app
+docker build --tag pickpix-app:$1 . --no-cache
+docker tag pickpix-app:$1 localhost:5000/pickpix-app:latest
+docker push localhost:5000/pickpix-app:latest
+#yq '.spec.template.spec.containers[0].image = "pickpix-app:$1"' -i pickpix-app-deployment.yaml
+kubectl replace -f pickpix-app-deployment.yaml --force
